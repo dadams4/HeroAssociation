e46f76b5edb006ea1d96a4a866715d19ec0aeebf
Daniel <dadams4@mail.umw.edu>
2017-01-30 18:02:42 +0000

    rudiment2

---
+++ cpsc350-project/siteCodeV1/msgboard.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS siteinfo;
+CREATE DATABASE  siteinfo;
+\c siteinfo;
+DROP TABLE IF EXISTS messageboard;
+CREATE TABLE messageboard (
+  id serial NOT NULL,
+  name varchar(35)  NOT NULL default '',
+  day date NOT NULL, 
+  age int NOT NULL default 0, 
+  message varchar(100) NOT NULL default '');
+
+INSERT INTO messageboard (name, day, age, message) VALUES ('Daniel Adams', '2017-01-24', 21, 'I am the creator of this site!');
+INSERT INTO messageboard (name, day, age, message) VALUES ('Ashley Otto', '2017-01-26', 22, 'I love this site!');
+INSERT INTO messageboard (name, day, age, message) VALUES ('George Washington', '2017-01-27', 285, 'I was the first President of the United States of America.');
+INSERT INTO messageboard (name, day, age, message) VALUES ('Benjamin Franklin', '2017-01-27', 311, 'I was never a President, but I am on the $100 bill!');
+INSERT INTO messageboard (name, day, age, message) VALUES ('Abraham Lincoln', '2017-01-28', 208, 'I was probably the tallest President of the United States.');
+INSERT INTO messageboard (name, day, age, message) VALUES ('Donald Trump', '2017-01-28', 70, 'I am the current President of the United States of America.');
--- cpsc350-project/siteCodeV1/server.py
+++ cpsc350-project/siteCodeV1/server.py
@@ -1,4 +1,6 @@
-import os
+#import os
+import psycopg2
+import psycopg2.extras
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -11,22 +13,66 @@ wallPosts = [{'name': 'Andrew Jackson', 'date': '2017-01-27', 'message': 'I was 
 
 @app.route('/')
 def mainIndex():
-	theWeek = "Week Two"
-	isWorking = False
+	theWeek = "Week Three"
+	isWorking = True
 	weeklySite = {'name': '/r/learnprogramming: ', 'description': 'A subreddit dedicated to learning and teaching programming'}
 	myVideos = [{'title': 'Judge Morty: State of Georgia Vs. Rick Allen(WARNING: BAD LANGUAGE', 'link': 'WTWdP5DMdsM','description': 'A real courtcase reenacted by Rick and Morty(WARNING: BAD LANGUAGE)'},
 	          {'title': 'Star Wars but every time someone shoots a laser it speeds up', 'link': 'wIIoMaidXq0', 'description': 'I needed a second video and this was suggested on the YouTube homepage'}]
 	return render_template('index.html', week = theWeek, site = weeklySite, working = isWorking, videos = myVideos)
 
 
-@app.route('/messages', methods = ['POST'])
+@app.route('/thankyou', methods = ['POST'])
 def sendmessage():
 	
-	wallPosts.append({'name': request.form['name'], 'date': request.form['date'], 'message': request.form['message'],
-		'email': request.form['email']})
+	conn = connectToDB()
+	cur = conn.cursor()
+#	name = request.form['name']
+#	day = request.form['date']
+#	age = request.form['age']
+#	msg = request.form['message']
+	try:
+		cur.execute("""INSERT INTO messageboard (name, day, age, message) 
+       VALUES (%s, %s, ,%d, %s);""",
+       (request.form['name'], request.form['date'], request.form['age'], request.form['message']) )
+#		cur.execute("insert into messageboard (name, day, age, message) VALUES (%s, %s, %d, %s)" % name , day, age, msg)
+	except:
+		print("error inserting")
+		conn.rollback()
+	
+	conn.commit()
+	
+	#return render_template('messageboard.html', posts = results)
+#	wallPosts.append({'name': request.form['name'], 'date': request.form['date'], 'message': request.form['message'],
+#		'email': request.form['email']})
 		
-	return render_template('sendinfo.html', yourName = request.form['name'], posts = wallPosts)
+	return render_template('sendinfo.html', yourName = request.form['name'])
+
+#Connecting to our database
+def connectToDB():
+	
+	connectionString = 'dbname=siteinfo user=manager password=Daniel21 host=localhost'
+	print connectionString
+	try:
+		return psycopg2.connect(connectionString)
+	except:
+		print("Unable to connect to the database")
+
+@app.route('/messageboard')
+def messageboard():
+	
+	connection = connectToDB()
+	cur = connection.cursor()
+	try:
+		cur.execute("select * from messageboard")
+	except:
+		print("An error has occurred while trying to display messages.")
+		
+	results = cur.fetchall()
+	return render_template('messageboard.html', posts=results)
 
 # start the server
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
+    
+    app.debug=True
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
